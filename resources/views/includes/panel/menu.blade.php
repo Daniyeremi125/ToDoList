dce2989c938c78c4507da505f70412a6d47d87a1
Daniyeremi12345 <202100352G@utea.edu.pe>
2024-07-25 00:04:14 -0500

    usuarios

--- resources/views/includes/panel/menu.blade.php
+++ resources/views/includes/panel/menu.blade.php
@@ -1,13 +1,15 @@
  <!-- Navigation -->
+ <h6 class="navbar-heading tex -muted"> PENDIENTES </h6>
+ 
  <ul class="navbar-nav">
     <li class="nav-item  active ">
       <a class="nav-link  active " href="./index.html">
-        <i class="ni ni-tv-2 text-primary"></i> Dashboard
+        <i class="ni ni-tv-2 text-info"></i> Dashboard
       </a>
     </li>
     <li class="nav-item">
       <a class="nav-link " href="./examples/icons.html">
-        <i class="ni ni-planet text-blue"></i> Icons
+        <i class="ni ni-book-bookmark text-blue"></i> Mis Tareas
       </a>
     </li>
     <li class="nav-item">
@@ -34,39 +36,10 @@
         <a class="nav-link" href="{{route('logout') }}"
             onclick="event.preventDefault(); document.getElementById('formLogout').submit();"
         >
-          <i class="fas fa-sign-in-alt"></i> cerrar sesion 
+          <i class="fas fa-sign-in-alt text-danger"></i> cerrar sesion 
         </a>
         <form action="{{ route('logout') }}" method="POST"  style="display: none;" id="formLogout">
             @csrf
         </form>
       </li>
     </ul>
-  <!-- Divider -->
-  <hr class="my-3">
-  <!-- Heading -->
-  <h6 class="navbar-heading text-muted">Documentation</h6>
-  <!-- Navigation -->
-  <ul class="navbar-nav mb-md-3">
-    <li class="nav-item">
-      <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/getting-started/overview.html">
-        <i class="ni ni-spaceship"></i> Getting started
-      </a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/foundation/colors.html">
-        <i class="ni ni-palette"></i> Foundation
-      </a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/components/alerts.html">
-        <i class="ni ni-ui-04"></i> Components
-      </a>
-    </li>
-  </ul>
-  <ul class="navbar-nav">
-    <li class="nav-item active active-pro">
-      <a class="nav-link" href="./examples/upgrade.html">
-        <i class="ni ni-send text-dark"></i> Upgrade to PRO
-      </a>
-    </li>
-  </ul>
